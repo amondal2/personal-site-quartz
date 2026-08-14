683a5def2fc97586e00e701d08d59228bc5ea13c
amondal2 <amondal13@gmail.com>
2026-08-14 15:47:55 -0700

    Quartz sync: Aug 14, 2026, 3:47 PM

--- quartz/components/Header.tsx
+++ quartz/components/Header.tsx
@@ -1,14 +1,18 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 export default (() => {
-  const Header: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
+  const Header: QuartzComponent = ({ cfg, ctx }: QuartzComponentProps) => {
+    const basePath = ctx.argv.serve || !cfg.baseUrl
+      ? ""
+      : new URL(`https://${cfg.baseUrl}`).pathname.replace(/\/$/, "")
+
     return (
       <header>
-        <img width="200px" src="/static/logo.svg" alt="" /> 
-        <h1><a href="/">{cfg.pageTitle || "Home"}</a></h1>
+        <img width="200px" src={`${basePath}/static/logo.svg`} alt="" />
+        <h1><a href={basePath || "/"}>{cfg.pageTitle || "Home"}</a></h1>
         <nav class="site-nav">
-          <a href="/">Home</a>
-          <a href="/posts">Writing</a>
+          <a href={basePath || "/"}>Home</a>
+          <a href={`${basePath}/posts`}>Writing</a>
         </nav>
       </header>
     )
